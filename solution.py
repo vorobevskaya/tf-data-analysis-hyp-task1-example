8bd15d3184b506cf2c4af9214f48985939714fef
vorobevskaya <113284505+vorobevskaya@users.noreply.github.com>
2023-04-11 15:40:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 689606612 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
